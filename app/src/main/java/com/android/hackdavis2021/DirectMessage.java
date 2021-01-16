859d870b422684b347d81ba1da4a9e4c717b665e
ho-eric <erho20@gmail.com>
2021-01-16 10:34:55 -0800

    Start dm functionality

---
+++ app/src/main/java/com/android/hackdavis2021/DirectMessage.java
@@ -0,0 +1,14 @@
+package com.android.hackdavis2021;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DirectMessage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_direct_message);
+    }
+}
